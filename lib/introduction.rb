e23f69e134eba2a9357d53ec1ae0458703a24723
Matthew Garrett <mdgarrett1984@gmail.com>
2017-12-26 03:59:09 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction(name, language)
+  puts "Hi, my name is #{name}."
 end
 
-introduction_with_language("Josh", "bob")
+introduction_with_language("Josh")
